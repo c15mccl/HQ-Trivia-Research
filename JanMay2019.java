bb5a7d64fa9a7d7803f059a0521ad4a713197916
mikeLostritto <ma25lost@siena.edu>
2019-05-20 11:16:39 -0400

    Set up for 2019 Jan to May HQBuff QA's

---
+++ JanMay2019.java
@@ -0,0 +1,75 @@
+import org.jsoup.nodes.*;
+import org.jsoup.*;
+import org.jsoup.select.*;
+import java.io.*;
+import java.util.*;
+
+/**
+ * Write a description of class JanMay2019 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class JanMay2019
+
+{
+    public static void main(String args[]){
+        String jan01 = " ";
+        String jan02 = " ";
+        String jan03 = " ";
+        String jan04 = " ";     
+        String jan05 = " ";     
+        String jan06 = " ";
+        String jan07 = " ";
+        String jan08 = " ";
+        String jan09 = " ";
+        String jan10 = " ";
+        String jan11 = " ";
+        String jan12 = " ";
+        String jan13 = " ";
+        String jan14 = " ";
+        String jan15 = " ";
+        String jan16 = " ";
+        String jan17 = " ";
+        String jan18 = " ";
+        String jan19 = " ";
+        String jan20 = " ";
+        String jan21 = " ";
+        String jan22 = " ";
+        String jan23 = " ";
+        String jan24 = " ";
+        String jan25 = " ";
+        String jan26 = " ";
+        String jan27 = " ";
+        String jan28 = " ";
+        String jan29 = " ";
+        String jan30 = " ";
+        String jan31 = " ";
+
+        String feb01 = " ";
+        String feb02 = " ";
+        String feb03 = " ";
+        String feb04 = " ";
+        String feb05 = " ";
+        String feb06 = " ";
+        String feb07 = " ";
+        String feb08 = " ";
+        String feb09 = " ";
+        String feb10 = " ";
+        String feb11 = " ";
+        String feb12 = " ";
+        String feb13 = " ";
+        String feb14 = " ";
+        String feb15 = " ";
+        String feb16 = " ";
+        String feb17 = " ";
+        String feb18 = " ";
+        String feb19 = " ";
+        String feb20 = " ";
+        String feb21 = " ";
+        String feb22 = " ";
+        String feb23 = " ";
+        String feb24 = " ";
+        //28 days in february 
+    }
+}
