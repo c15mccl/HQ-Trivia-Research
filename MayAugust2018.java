f0842a2a4bf08b198dfe4f43a0515d882365e1d8
c15mccl <c15mccl@siena.edu>
2019-05-22 11:03:37 -0400

    Making changes to MAyAugust.java

--- MayAugust2018.java
+++ MayAugust2018.java
@@ -3,6 +3,7 @@ import org.jsoup.*;
 import org.jsoup.select.*;
 import java.io.*;
 import java.util.*;
+import java.nio.file.*;
 /**
  * Class that ggrabsthe data from hqbuff for the months of May-August of 2018. 
  *
@@ -11,33 +12,32 @@ import java.util.*;
  */
 public class MayAugust2018
 {
-    public static void main(String[]args)throws FileNotFoundException{
+    public static void main(String[]args)throws FileNotFoundException, IOException, InterruptedException {
         //May
         File myFile = new File("MAYAUG.txt");
         Scanner scan = new Scanner(myFile);
         while(scan.hasNextLine()){
             String URL = scan.nextLine();
             
-            
-            
             try{
-                File outFile = new File(URL + ".txt");
-                PrintWriter output = new PrintWriter(outFile);
+                FileWriter fwrite =  new FileWriter(URL + ".txt");
+                BufferedWriter writer = new BufferedWriter(fwrite);
                 Document hQdata = Jsoup.connect(URL).get();
                 Elements hQdates = hQdata.select("script");
                
                 for(Element e : hQdates ){
                    for (DataNode node : e.dataNodes()){
                        System.out.println(node.getWholeData());
-                       output.println(node.getWholeData());
+                       writer.write(node.getWholeData());
                     }
                 }
                 System.out.println("------------");
-                output.close();
+                writer.close();
                 Thread.sleep(1000);
             }
             catch (Exception e){
                 e.printStackTrace();
+                //System.err.println(x);
                 System.exit(1);
             }
         }
