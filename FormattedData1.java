a7ce3c7ae162fd7c96538006fbdeca1df28ad6a0
c15mccl <c15mccl@siena.edu>
2019-05-28 15:59:05 -0400

    Resolving conflicts

--- FormattedData1.java
+++ FormattedData1.java
@@ -18,21 +18,23 @@ public class FormattedData1
         System.out.println("START");
         int a = 1;
         int b = 1;
+        //((\\w+\\s){1,})\\?
         try{
             File myfile = new File("MayAugList.txt");
             FileWriter fwrite = new FileWriter(myfile);
             PrintWriter pwrite =  new PrintWriter(fwrite);
-            String firstQues = "What
+            String firstQues = "Which of these is a common kind";
             String patternMatch = "";
-            while (scan.hasNextLine()){
+            String ques = scan.nextLine();   
+            while ((ques.equals(firstQues)) == false){
 
                 //System.out.println("***QUESTIONS***");    
-                Pattern p1 = Pattern.compile("\"((\\w+\\s){1,})\\?(\\s+)");
+                Pattern p1 = Pattern.compile("\"text\":\"((\\w+\\s){1,})\\?");
                 Matcher m1 = p1.matcher(scan.nextLine());
                 while (m1.find()){
                     System.out.print("Question " +(a++)+":");
                     patternMatch = m1.group();
-                    pwrite.println("Question " +(a++)+":");
+                    //pwrite.println("Question " +(a++)+":");
                     System.out.println(patternMatch);
                     pwrite.println(patternMatch);
 
@@ -40,42 +42,41 @@ public class FormattedData1
                 }
             }
             /*while (scan.hasNextLine()){           
-                Pattern p2 = Pattern.compile("\"category\"(.{1,10})\"\\,{9}");
-                Matcher m2 = p2.matcher(scan.nextLine());
+            Pattern p2 = Pattern.compile("\"category\"(.{1,10})\"\\,{9}");
+            Matcher m2 = p2.matcher(scan.nextLine());
 
-                while (m2.find()){
+            while (m2.find()){
 
-                    patternMatch = m2.group();
-                    System.out.println(patternMatch);
-                    pwrite.println(patternMatch);
-                    System.out.println("--------------------------------");
-                }
+            patternMatch = m2.group();
+            System.out.println(patternMatch);
+            pwrite.println(patternMatch);
+            System.out.println("--------------------------------");
+            }
             }
             while (scan.hasNextLine()){  
-                Pattern p3 = Pattern.compile("\"answers\"(.{200})\"");
-                Matcher m3 = p3.matcher(scan.nextLine());
-                while (m3.find()){
-                    System.out.print("Answer " +(b++)+"-"+(b+1)+":");
-                    patternMatch = m3.group();
-                    System.out.println(patternMatch);
-                    pwrite.println("--------------------------------");
-                    pwrite.println(patternMatch);
-                    pwrite.println("--------------------------------");
-                    System.out.println("--------------------------------");
-                }
+            Pattern p3 = Pattern.compile("\"answers\"(.{200})\"");
+            Matcher m3 = p3.matcher(scan.nextLine());
+            while (m3.find()){
+            System.out.print("Answer " +(b++)+"-"+(b+1)+":");
+            patternMatch = m3.group();
+            System.out.println(patternMatch);
+            pwrite.println("--------------------------------");
+            pwrite.println(patternMatch);
+            pwrite.println("--------------------------------");
+            System.out.println("--------------------------------");
+            }
 
             }
             while (scan.hasNextLine()){
-                Pattern p4 = Pattern.compile("\"savage\":(\\w++)");
-                Matcher m4 = p4.matcher(scan.nextLine());
-                while (m4.find()){
-                    patternMatch = m4.group();
-                    System.out.println(patternMatch);
-                    pwrite.println(patternMatch);
-                    System.out.println("--------------------------------");
-                }
+            Pattern p4 = Pattern.compile("\"savage\":(\\w++)");
+            Matcher m4 = p4.matcher(scan.nextLine());
+            while (m4.find()){
+            patternMatch = m4.group();
+            System.out.println(patternMatch);
+            pwrite.println(patternMatch);
+            System.out.println("--------------------------------");
+            }
             }*/
-            
 
             fwrite.close();
             pwrite.close();
