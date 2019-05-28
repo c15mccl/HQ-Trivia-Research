23b58bb2c5651475c99c94c12d22b4c58cc7fa64
c15mccl <c15mccl@siena.edu>
2019-05-28 13:23:35 -0400

    Updating FormattedData1.java

--- FormattedData1.java
+++ FormattedData1.java
@@ -16,38 +16,31 @@ public class FormattedData1
         File file = new File("MayAugQuestions.txt");
         Scanner scan = new Scanner(file);
         System.out.println("START");
+        int a = 1;
+        int b = 1;
         try{
             File myfile = new File("MayAugList.txt");
             FileWriter fwrite = new FileWriter(myfile);
             PrintWriter pwrite =  new PrintWriter(fwrite);
+            String patternMatch = "";
+            while (scan.hasNext()){
 
-            while (scan.hasNextLine()){
-
-                //Pattern p1 = Pattern.compile("\"((\\w+\\s){1,})\\?");
-                //Matcher m1 = p1.matcher(scan.nextLine());
-
-                //Pattern p2 = Pattern.compile("\"category\"(.{1,10})\"\\,{9}");
-                //Matcher m2 = p2.matcher(scan.nextLine());
-                //System.out.println("***ANSWERS***");
-                Pattern p3 = Pattern.compile("\"answers\"(.{200})\"");
-                Matcher m3 = p3.matcher(scan.nextLine());
-                //Pattern p4 = Pattern.compile("\"savage\":(\\w++)");
-                //Matcher m4 = p4.matcher(scan.nextLine());
-                //Pattern p5 = Pattern.compile("\"savage\":(\\w++)");
-                //Matcher m5 = p5.matcher(scan.nextLine());
-                String str;
-                //Matcher m = p.matcher(str = scan.nextLine());
-                //System.out.println("->" + str + "<-");
-                String patternMatch = "";
-                //System.out.println("***QUESTIONS***");                    
-                /*while (m1.find()){
-
+                //System.out.println("***QUESTIONS***");    
+                Pattern p1 = Pattern.compile("\"text\":\"((\\w+\\s){1,})\\?");
+                Matcher m1 = p1.matcher(scan.nextLine());
+                while (m1.find()){
+                    System.out.print("Question " +(a++)+":");
                     patternMatch = m1.group();
                     System.out.println(patternMatch);
                     pwrite.println(patternMatch);
+
                     System.out.println("--------------------------------");
                 }
-                System.out.println("***CATEGORIES***");
+            }
+            while (scan.hasNext()){           
+                Pattern p2 = Pattern.compile("\"category\"(.{1,10})\"\\,{9}");
+                Matcher m2 = p2.matcher(scan.nextLine());
+
                 while (m2.find()){
 
                     patternMatch = m2.group();
@@ -55,28 +48,32 @@ public class FormattedData1
                     pwrite.println(patternMatch);
                     System.out.println("--------------------------------");
                 }
-                
-                */
+            }
+            while (scan.hasNext()){  
+                Pattern p3 = Pattern.compile("\"answers\"(.{200})\"");
+                Matcher m3 = p3.matcher(scan.nextLine());
                 while (m3.find()){
+                    System.out.print("Answer " +(b++)+"-"+(b+1)+":");
                     patternMatch = m3.group();
                     System.out.println(patternMatch);
+                    pwrite.println("--------------------------------");
                     pwrite.println(patternMatch);
+                    pwrite.println("--------------------------------");
                     System.out.println("--------------------------------");
                 }
-                /*
+
+            }
+            while (scan.hasNext()){
+                Pattern p4 = Pattern.compile("\"savage\":(\\w++)");
+                Matcher m4 = p4.matcher(scan.nextLine());
                 while (m4.find()){
                     patternMatch = m4.group();
                     System.out.println(patternMatch);
                     pwrite.println(patternMatch);
                     System.out.println("--------------------------------");
                 }
-                while (m5.find()){
-                    patternMatch = m5.group();
-                    System.out.println(patternMatch);
-                    pwrite.println(patternMatch);
-                    System.out.println("--------------------------------");
-                }*/
             }
+
             fwrite.close();
             pwrite.close();
         }
@@ -87,3 +84,4 @@ public class FormattedData1
         }
     }
 }
+
