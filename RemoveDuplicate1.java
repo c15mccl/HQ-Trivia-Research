b1d0dbfe22e6045b7b42bbd8902f47463c02e598
c15mccl <c15mccl@siena.edu>
2019-05-29 15:55:12 -0400

    Saving all work for the day

--- RemoveDuplicate1.java
+++ RemoveDuplicate1.java
@@ -13,28 +13,33 @@ import java.util.regex.*;
 public class RemoveDuplicate1
 {
     public static void main(String[]args) throws IOException{
-        File myfile2 = new File("MayAugList.txt");
-        Scanner in = new Scanner(myfile2);
+        File file = new File("MayAugList.txt");
+        Scanner in = new Scanner(file);
+        ArrayList <String> questions = new ArrayList <String>();
         try{
+            File myfile2 = new File("MayAugFinal.txt");
             FileWriter fwrite2 = new FileWriter(myfile2);
             PrintWriter pwrite2 =  new PrintWriter(fwrite2);
-            String finder = in.nextLine();
-            ArrayList <String> questions = new ArrayList <String>();
             while (in.hasNextLine()){
+                String finder = in.nextLine();
+                //System.out.println(finder);
                 questions.add(finder);
-                for(int i = 0; i < questions.size() -1; i++){
-                    for(int j = 0; j < i; j++){
-                        System.out.println(questions.get(i));
-                        if(questions.get(i).equals(questions.get(j))){
-                            questions.remove(questions.get(i));
-                        }
-                    }
-                    System.out.println(questions);
-                    pwrite2.println(questions);
-                }
             }
+            for(int i = 0; i < questions.size() -1; i++){
+                for(int j = 0; j < i; j++){
+
+                    if(questions.get(i).equals(questions.get(j))){
+                        questions.remove(questions.get(i));
+                    }
+                }
+                System.out.println(questions);
+                pwrite2.println(questions);
+            }
+
             fwrite2.close();
             pwrite2.close();
+
+            System.out.println(questions);
         }
         catch (Exception e){
             e.printStackTrace();
