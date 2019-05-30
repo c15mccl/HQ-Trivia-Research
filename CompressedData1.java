aa3099d11f17163f6d6405a5618ea5e778270dfc
c15mccl <c15mccl@siena.edu>
2019-05-30 10:47:50 -0400

    Working on removing duplicates

--- CompressedData1.java
+++ CompressedData1.java
@@ -34,7 +34,7 @@ public class CompressedData1
                     String patternMatch = "";
                     while (m.find()){
                         patternMatch = m.group();
-                        String question = patternMatch.replaceAll("
+                        String question = patternMatch.replaceAll("\\?"," ?");
                         System.out.println(patternMatch);
                         pwrite.println(patternMatch);
                         System.out.println();
