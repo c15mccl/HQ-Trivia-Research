8cb7069df2360c53c8a55c671f3a546da95d61e4
Rachel Cleary <rcleary@Rachels-MacBook-Air.local>
2019-06-03 21:54:00 -0400

    janmay2019 fixed

--- JanMay2019.java
+++ JanMay2019.java
@@ -13,42 +13,46 @@ import java.util.*;
 public class JanMay2019
 
 {
-
-    public static void main(String args[]) throws FileNotFoundException {
+    public static void main(String [] args) throws FileNotFoundException
+    {
         File file = new File("JanMay2019.txt");
+
         Scanner scan = new Scanner(file);
+        int dayNum = 1;
+        String day = "Day: ";
         while(scan.hasNextLine()){
             String URL = scan.nextLine();
-            System.out.println(URL);
-            String parse1 = URL.substring(27,37);
-            String parse2 = URL.substring(38);
-            String parse3 = "-";
-            String parse4 = parse1 + parse3 + parse2;
-            String filename = parse4 + ".txt";
+            //System.out.println(URL);
+            String filename = day + dayNum;
             System.out.println(filename);
             try{
-                FileWriter fwrite =  new FileWriter(filename);
-                BufferedWriter writer = new BufferedWriter(fwrite);
+                File myFile = new File(filename);
+                FileWriter fwrite =  new FileWriter(myFile);
+                PrintWriter pwrite = new PrintWriter(fwrite);
                 Document hQdata = Jsoup.connect(URL).get();
                 Elements hQdates = hQdata.select("script");
+                //Pattern p = Pattern.compile("\"text\":\"");
+
                 for (Element e : hQdates)
                     for(DataNode node: e.dataNodes())
                     {
                         //System.out.println(node.getWholeData());
-                        writer.write(node.getWholeData());
+                        pwrite.println(node.getWholeData());
                     }
 
                 System.out.println("------------");
-                //writer.close();
+                fwrite.close();
+                pwrite.close();
                 Thread.sleep(1000);
+
             }
             catch (Exception e){
                 e.printStackTrace();
                 //System.err.println(x);
                 System.exit(1);
             }
+            dayNum++;
         }
-
 
     }
 }
