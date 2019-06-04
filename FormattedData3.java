956f47dab6091a7933eddf5bf95c7694c14f2625
c15mccl <c15mccl@siena.edu>
2019-06-03 21:46:30 -0400

    FormattedData3.java

--- FormattedData3.java
+++ FormattedData3.java
@@ -13,63 +13,24 @@ import java.util.regex.*;
 public class FormattedData3
 {
     public static void main(String args[]) throws IOException{
-        File file = new File("JanMay2019Questions.txt");
+        File file = new File("MayAugScript.txt");
         Scanner scan = new Scanner(file);
-        System.out.println("Start");
+        //System.out.println("Start");
         try{
-            File myfile = new File("JanMayList.txt");
+            File myfile = new File("JanMayAnsCat1.txt");
             FileWriter fwrite = new FileWriter(myfile);
             PrintWriter pwrite =  new PrintWriter(fwrite);
-
-            while (scan.hasNextLine()){
-                //Pattern p1 = Pattern.compile("\"((\\w+\\s){1,})\\?");
-                //Matcher m1 = p1.matcher(scan.nextLine());
-                //Pattern p2 = Pattern.compile("\"category\"(.{1,10})\",");
-                //Matcher m2 = p2.matcher(scan.nextLine());
-                Pattern p3 = Pattern.compile("\"answers\":[{\"text\":\"(.{1})\"}]");
-                Matcher m3 = p3.matcher(scan.nextLine());
-                //Pattern p4 = Pattern.compile("\"savage\":(\\w++)");
-                //Matcher m4 = p4.matcher(scan.nextLine());
-                //Pattern p5 = Pattern.compile("\"savage\":(\\w++)");
-                //Matcher m5 = p5.matcher(scan.nextLine());
-                String str;
-                //Matcher m = p.matcher(str = scan.nextLine());
-                //System.out.println("->" + str + "<-");
-                String patternMatch = "";
-
-                /*while (m1.find()){
+            String patternMatch = "";
+            while (scan.hasNextLine()){   
+                Pattern p1 = Pattern.compile("\"text\":\"(((([^? ])+\\s){1,})\\?)");
+                Matcher m1 = p1.matcher(scan.nextLine());
+                while (m1.find()){
                     patternMatch = m1.group();
+                    //String question = patternMatch.replaceAll("text","Question");                   
                     System.out.println(patternMatch);
                     pwrite.println(patternMatch);
                     System.out.println("--------------------------------");
                 }
-                while (m2.find()){
-                    patternMatch = m2.group();
-                    System.out.println(patternMatch);
-                    pwrite.println(patternMatch);
-                    System.out.println("--------------------------------");
-                }
-                */
-                
-                while (m3.find()){
-                    patternMatch = m3.group();
-                    System.out.println(patternMatch);
-                    pwrite.println(patternMatch);
-                    System.out.println("--------------------------------");
-                }
-                /*
-                while (m4.find()){
-                    patternMatch = m4.group();
-                    System.out.println(patternMatch);
-                    pwrite.println(patternMatch);
-                    System.out.println("--------------------------------");
-                }
-                while (m5.find()){
-                    patternMatch = m5.group();
-                    System.out.println(patternMatch);
-                    pwrite.println(patternMatch);
-                    System.out.println("--------------------------------");
-                }*/
             }
             fwrite.close();
             pwrite.close();
