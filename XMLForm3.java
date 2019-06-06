c3de4e437059a3e9232724349fc0122db649cfd3
mikeLostritto <ma25lost@siena.edu>
2019-06-06 10:13:43 -0400

    XML date setup

---
+++ XMLForm3.java
@@ -0,0 +1,58 @@
+import org.jsoup.nodes.*;
+import org.jsoup.*;
+import org.jsoup.select.*;
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+/**
+ * Write a description of class XMLForm3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class XMLForm3
+{
+    public static void main (String[]args) throws FileNotFoundException{
+        File file = new File("JanMayFinal.txt");
+        Scanner scan = new Scanner(file);
+        try{
+            File myfile = new File("XMLForm3.txt");
+            FileWriter fwrite = new FileWriter(myfile);
+            PrintWriter pwrite =  new PrintWriter(fwrite);
+            while(scan.hasNextLine()){
+                String line = scan.nextLine();
+                if(line.contains("\"text\":")){
+                    line = line.replace("\"text\":","<Question> ");
+                }
+                if(line.contains("?")){
+                    line = line + " </Question>";
+                }
+                if(line.contains("Answers:")){
+                    line = line.replace("Answers:","<Answer>");
+                    line = line + " </Answer>";
+                }
+                if(line.contains("Category: \"category\":")){
+                    line = line.replace("Category: \"category\":","<Category cat = ");
+                    line = line + "> </Category>";
+                }
+                if(line.contains("Savage \"savage\":")){
+                    line = line.replace("Savage \"savage\":","<Savage level = ");
+                    line = line + "> </Savage>";
+                }
+                if(line.contains("Dates: ")){
+                    line = line.replace("Dates:","<Dates>");
+                    line = line + " </Dates>";
+                }
+                System.out.println(line);
+                pwrite.println(line);
+            }
+
+            pwrite.close();
+            fwrite.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
