4bc3df1fa99b05cd8f324b35dc01e4d13d8f153f
Rachel Cleary <rcleary@Rachels-MacBook-Air.local>
2019-06-06 09:46:00 -0400

    JanMaySav.java

---
+++ JanMaySav.java
@@ -0,0 +1,42 @@
+import org.jsoup.nodes.*;
+import org.jsoup.*;
+import org.jsoup.select.*;
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+/**
+ * Write a description of class JanMaySav here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class JanMaySav
+{
+    public static void main(String [] args) throws FileNotFoundException
+    {
+        File file = new File("JanMayScript.txt");
+        Scanner in = new Scanner(file);
+        try{
+            File file2 = new File("JanMaySav.txt");
+            FileWriter fwrite2 = new FileWriter(file2);
+            PrintWriter pwrite2 =  new PrintWriter(fwrite2);
+            String patternMatch = "";
+            while(in.hasNextLine()){
+               String line = in.nextLine();
+                Pattern p = Pattern.compile("\"savage\":(\\w{1,})");
+                Matcher m = p.matcher(line);
+                while (m.find()){
+                    patternMatch = m.group();
+                    System.out.println(patternMatch);
+                    pwrite2.println(patternMatch);
+                }
+            }
+            fwrite2.close();
+            pwrite2.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
