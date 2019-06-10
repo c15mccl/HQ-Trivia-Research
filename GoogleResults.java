a7a6daeb741d35f1e184bd00fb81d9db3c0c40d9
c15mccl <c15mccl@siena.edu>
2019-06-10 15:51:58 -0400

    Finishing GoogleResults.java

--- GoogleResults.java
+++ GoogleResults.java
@@ -14,24 +14,16 @@ import java.util.regex.*;
 public class GoogleResults
 {
     public static void main(String[]args)throws FileNotFoundException, IOException, InterruptedException {
-            String URL = "https://www.google.com/search?gs_ssp=eJzj4tLP1TcwKzcqzjM1YAQAFpkDNg&q=justin+bieber&oq=jus&aqs=chrome.2.69i57j0j46l3j0.2859j0j8&sourceid=chrome&ie=UTF-8";
+            String URL = "https://www.google.com/search?q=when+was+siena+college+founded&oq=when+was+siena+college+founded&aqs=chrome..69i57.28499j1j4&sourceid=chrome&ie=UTF-8";
             try{
                 File myfile = new File("Results.txt");
                 FileWriter fwrite = new FileWriter(myfile);
                 PrintWriter pwrite =  new PrintWriter(fwrite);
-                Document hQdata = Jsoup.connect(URL).get();
-                Elements hQdates = hQdata.select("id");
+                Document doc = Jsoup.connect(URL).get();
+                Elements elem = doc.select("div#resultStats");
+                System.out.println(elem);
 
-                for(Element e : hQdates ){
-                    for (DataNode node : e.dataNodes()){
-                        pwrite.println(node.getWholeData());
-                    }
-
-                }
-                System.out.println("------------");
-                fwrite.close();
-                pwrite.close();
-                Thread.sleep(1000);
+                
             }
             catch (Exception e){
                 e.printStackTrace();
