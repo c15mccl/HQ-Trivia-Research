724262538e42e102bbeb1b3888255c9c05b96313
c15mccl <c15mccl@siena.edu>
2019-06-16 13:11:44 -0400

    Saving changes done over the weekend

--- SavageLearning1.java
+++ SavageLearning1.java
@@ -17,7 +17,7 @@ public class SavageLearning1
         Scanner in = new Scanner(myfile);
         ArrayList <String> alldata = new ArrayList <String>();
         ArrayList <String> countString = new ArrayList <String>();
-        ArrayList <String> countInt = new ArrayList <String>();
+        ArrayList <Integer> countInt = new ArrayList <Integer>();
         int aCount = 0;
         int bCount = 0;
         int cCount = 0;
@@ -30,6 +30,7 @@ public class SavageLearning1
         int total = 0;
         String patternMatch = "";
         String helperString1  = "";
+        int helperNum1 = 0;
         String helperString2  = "";
         String helperString3  = "";
         while(in.hasNextLine()){
@@ -50,8 +51,20 @@ public class SavageLearning1
             for(int i = 0; i < countString.size(); i++){
                 helperString1 = countString.get(i);
                 helperString2 = helperString1.substring(0,9);
-                helperString1 = Integer.valueOf(helperString1);
-                countInt.add(helperString1);
+                helperNum1 = Integer.valueOf(helperString1);
+                countInt.add(helperNum1);
+            }
+            for(int i = 0; i < countInt.size(); i++){
+                for(int j = 0; j < i; j+=3){
+                    for(int k = 1; k < j; k++){
+                        for(int l = 2; l < k; l++){
+                            count1 = countInt.get(j);
+                            count2 = countInt.get(k);
+                            count3 = countInt.get(l);
+                            total = count1 + count2 + count3;
+                        }
+                    }
+                }                
             }
         }
     }
