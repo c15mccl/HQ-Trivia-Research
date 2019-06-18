8c64f415fc88e42d132c54301f1450c97fe3f17a
c15mccl <c15mccl@siena.edu>
2019-06-18 12:49:41 -0400

    Pushing changes to SavageLearning1.java

--- SavageLearning1.java
+++ SavageLearning1.java
@@ -29,62 +29,69 @@ public class SavageLearning1
         int prevBcount = 0;
         int prevCcount = 0;
         int total = 0;
+        String tempString = "";
         String patternMatch = "";
         String helperString1  = "";
         int helperNum1 = 0;
         String helperString2  = "";
         String helperString3  = "";
+        CharSequence comma = ",";
         while(in.hasNextLine()){
             String line = in.nextLine();
             if(line.contains("<answer")){
                 alldata.add(line);
             }
-            for(int i = 0; i < alldata.size(); i++){
-                Pattern p = Pattern.compile("count\":\"([0-9,]+)");
-                Matcher match = p.matcher(alldata.get(i));
+        }
+        for(int i = 0; i < alldata.size(); i++){
+            Pattern p = Pattern.compile("count\":\"([0-9,]+)");
+            Matcher match = p.matcher(alldata.get(i));
 
-                while(match.find()){
-                    patternMatch = match.group();
-                    countString.add(patternMatch);
-
-                }
-                for(int j = 0; j < countString.size(); j++){
-                    helperString1 = countString.get(j);
-                    helperString2 = helperString1.substring(0,9);
-                    helperNum1 = Integer.valueOf(helperString1);
-                    countInt.add(helperNum1);
-                }
-                for(int k = 0; k < countInt.size(); k++){
-                    for(int l = 0; l < k; l+=3){
-                        for(int m = 1; m < l; m++){
-                            for(int n = 2; n < m; n++){
-                                count1 = countInt.get(l);
-                                count2 = countInt.get(m);
-                                count3 = countInt.get(n);
-                                total = count1 + count2 + count3;
-                                if(count1 < (total/3)){
-                                    if((alldata.get(i)).contains("correct = true")){
-                                        aCount++;
-                                    }
-
-                                    //||(count3 < (total/3))){
-
-                                }
-                                else if(count2 < (total/3)){
-                                    if((alldata.get(i)+1).contains("correct = true")){
-                                        bCount++;
-                                    }
-                                }
-                                else{
-                                    cCount++; 
-                                }
+            while(match.find()){
+                patternMatch = match.group();
+                //patternMatch = patternMatch.substring(8,(patternMatch.length()));
+                StringBuilder sb = new StringBuilder(patternMatch);
+                sb.delete(0,8);
+                String regex = "(?<=[\\d])(,)(?=[\\d])";
+                Pattern q = Pattern.compile(regex);
+                Matcher m = q.matcher(sb);
+                tempString = m.replaceAll("");
+                countString.add(tempString);
+            }
+        }
+        for(int i = 0; i < alldata.size(); i++){
+            for(int j = 0; j < countString.size(); j++){
+                helperString1 = countString.get(j);
+                helperNum1 = Integer.valueOf(helperString1);
+                countInt.add(helperNum1);
+            }
+            for(int k = 0; k < countInt.size(); k+=3){
+                count1 = countInt.get(k);
+                for(int l = 1; l < (k+2); l+=3){
+                    count2 = countInt.get(l);
+                    for(int m = 2; m < (l+2); m+=3){
+                        count3 = countInt.get(m);
+                        total = count1 + count2 + count3;
+                        if(count1 < (total/3)){
+                            if((alldata.get(i)).contains("correct = true")){
+                                aCount++;
                             }
                         }
-                    }                
-                }
+                        else if(count2 < (total/3)){
+                            if((alldata.get(i)+1).contains("correct = true")){
+                                bCount++;
+                            }
+                        }
+                        else if (count3 < (total/3)){
+                            cCount++; 
+                        }
+                        System.out.println("DONE");
+                    }
+                }                
             }
-
         }
+        System.out.println("A was the correct answer for a savage question "+aCount+" times.");
+        System.out.println("B was the correct answer for a savage question "+bCount+" times.");
+        System.out.println("C was the correct answer for a savage question "+cCount+" times.");
     }
-
 }
+
